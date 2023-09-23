3bb26f1578268922c1e5c2f25776fe029e8c4071
D-Pats <devinpatel97@gmail.com>
2023-09-23 19:28:49 +0100

    changed chord fingering to a table to better represent which string each fringer is for

--- frontend/script.js
+++ frontend/script.js
@@ -109,7 +109,44 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 
   function createFingeringChart() {
-    chordFingeringWrite.textContent = chordsData[getRootNote()].fingering;
+    //https://developer.mozilla.org/en-US/docs/Web/API/Document_Object_Model/Traversing_an_HTML_table_with_JavaScript_and_DOM_Interfaces
+    // creates a <table> element and a <tbody> element
+    const tbl = document.createElement("table");
+    const tblBody = document.createElement("tbody");
+
+    // creating all cells
+    for (let i = 0; i < 2; i++) {
+      // creates a table row
+      const row = document.createElement("tr");
+
+      for (let j = 0; j < 6; j++) {
+        // Create a <td> element and a text node, make the text
+        // node the contents of the <td>, and put the <td> at
+        // the end of the table row
+        const cell = document.createElement("td");
+        if (i === 0) {
+          const cellText = document.createTextNode(`${allStrings[j]}`);
+          cell.appendChild(cellText);
+          row.appendChild(cell);
+        } else {
+          const cellText = document.createTextNode(
+            `${chordsData[getRootNote()].fingering[j]}`
+          );
+          cell.appendChild(cellText);
+          row.appendChild(cell);
+        }
+      }
+
+      // add the row to the end of the table body
+      tblBody.appendChild(row);
+    }
+
+    // put the <tbody> in the <table>
+    tbl.appendChild(tblBody);
+    // appends <table> into <body>
+    chordFingeringWrite.appendChild(tbl);
+    // sets the border attribute of tbl to '2'
+    tbl.setAttribute("border", "1");
   }
 
   function createTextChord() {
