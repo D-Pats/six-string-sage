32771dac6596f331a08baf8660248172f7d51965
D-Pats <devinpatel97@gmail.com>
2023-09-24 17:18:56 +0100

    added table to represent whole fretboard and a scale-diagram class to assign to the element holding it, also added the string names and fret numbers

--- frontend/script.js
+++ frontend/script.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const chordSelect = document.getElementById("chord-select");
   const chordFingeringWrite = document.querySelector(".chord-fingering");
   const chordNotesWrite = document.querySelector(".chord-notes");
+  const scaleDiagramDisplay = document.querySelector(".scale-diagram");
 
   const chordsData = [
     {
@@ -96,6 +97,35 @@ document.addEventListener("DOMContentLoaded", function () {
 
   const allStrings = ["e", "B", "G", "D", "A", "E"];
 
+  const allNotesTwo = [
+    "A",
+    "A#",
+    "B",
+    "C",
+    "C#",
+    "D",
+    "D#",
+    "E",
+    "F",
+    "F#",
+    "G",
+    "G#",
+    "A",
+    "A#",
+    "B",
+    "C",
+    "C#",
+    "D",
+    "D#",
+    "E",
+    "F",
+    "F#",
+    "G",
+    "G#",
+  ];
+
+  const majorSteps = [2, 2, 1, 2, 2, 2, 1];
+
   //Functions
 
   function getRootNote() {
@@ -157,6 +187,51 @@ document.addEventListener("DOMContentLoaded", function () {
     chordNotesWrite.textContent = chordsData[getRootNote()].scaleNotes;
   }
 
+  function createScaleChart() {
+    //removes existing table if switching chords
+    scaleDiagramDisplay.textContent = "";
+
+    // creates a <table> element and a <tbody> element
+    const tbl = document.createElement("table");
+    const tblBody = document.createElement("tbody");
+
+    // creating all cells
+    for (let i = 0; i < 7; i++) {
+      // creates a table row
+      const row = document.createElement("tr");
+
+      for (let j = 0; j < 25; j++) {
+        // Create a <td> element and a text node, make the text
+        // node the contents of the <td>, and put the <td> at
+        // the end of the table row
+        const cell = document.createElement("td");
+        if (j === 0 && i === 0) {
+          const cellText = document.createTextNode(`Fret`);
+          cell.appendChild(cellText);
+          row.appendChild(cell);
+        } else if (j === 0) {
+          const cellText = document.createTextNode(`${allStrings[i - 1]}`);
+          cell.appendChild(cellText);
+          row.appendChild(cell);
+        } else if (i === 0) {
+          const cellText = document.createTextNode(`${j}`);
+          cell.appendChild(cellText);
+          row.appendChild(cell);
+        }
+      }
+
+      // add the row to the end of the table body
+      tblBody.appendChild(row);
+    }
+
+    // put the <tbody> in the <table>
+    tbl.appendChild(tblBody);
+    // appends <table> into <body>
+    scaleDiagramDisplay.appendChild(tbl);
+    // sets the border attribute of tbl to '2'
+    tbl.setAttribute("border", "1");
+  }
+
   //Event listeners
 
   //Event listener to change the fingering and notes for scale that show when selecting different root notes from the dropdown
@@ -164,9 +239,11 @@ document.addEventListener("DOMContentLoaded", function () {
     getRootNote();
     createFingeringChart();
     createTextChord();
+    createScaleChart();
   });
 
   //Running functions on initial load
   createFingeringChart();
   createTextChord();
+  createScaleChart();
 });
